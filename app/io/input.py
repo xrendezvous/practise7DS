618da39126106ebede4e4c30739733378d21f267
Aryna Reutska <arina.reutskaya110@gmail.com>
2024-03-17 16:36:51 +0200

    updated files

---
+++ app/io/input.py
@@ -0,0 +1,11 @@
+def input_text():
+    pass
+
+
+def read_file(file_name):
+    pass
+
+
+def read_file_pandas(file_name):
+    pass
+
---
+++ app/io/output.py
@@ -0,0 +1,11 @@
+def output_text(text):
+    pass
+
+
+def write_file(file_name, text):
+    pass
+
+
+def write_file_pandas(file_name, text):
+    pass
+
